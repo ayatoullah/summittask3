11a94e3cb34d9229a95e16bd31752cab01a963fd
ayatoullah <ayaramzy2014@gmail.com>
2018-12-15 12:43:37 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ClientdataComponent } from './clientdata/clientdata.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    ClientdataComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/clientdata/clientdata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientdata',
+  templateUrl: './clientdata.component.html',
+  styleUrls: ['./clientdata.component.css']
+})
+export class ClientdataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
